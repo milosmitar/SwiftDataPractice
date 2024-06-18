b97a62902d07916d4661f687f9e30c23960e92ad
Milos <m.mitrovic@itsinbox.com>
2024-06-18 11:36:06 +0200

    Delete.

--- SwiftDataPractice/Views/ReviewListView.swift
+++ SwiftDataPractice/Views/ReviewListView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct ReviewListView: View {
     @Environment(\.modelContext) private var context
-    let reviews: [Review]
+//    let reviews: [Review]
+    let movie: Movie
     
     
     var body: some View {
         List{
-            ForEach(reviews){ review in
+            ForEach(movie.reviews){ review in
                 
                 VStack(alignment: .leading){
                     Text(review.subject)
@@ -27,10 +28,11 @@ struct ReviewListView: View {
     }
     func deleteReview(indexSet: IndexSet){
         indexSet.forEach { index in
-            let review = reviews[index]
+            let review = movie.reviews[index]
             context.delete(review)
             
             do{
+                movie.reviews.remove(at: index)
                 try context.save()
             }catch{
                 print(error.localizedDescription)
@@ -39,7 +41,7 @@ struct ReviewListView: View {
     }
 }
 
-#Preview {
-    ReviewListView(reviews: [])
-        .modelContainer(for: [Review.self, Movie.self])
-}
+//#Preview {
+//    ReviewListView(reviews: [])
+//        .modelContainer(for: [Review.self, Movie.self])
+//}
